435b97ac9de953ce1318f69631388c66f56dcfcc
Yurii Ziatyk <121460368+Yurii056@users.noreply.github.com>
2023-04-05 17:59:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const fs = require("fs")
     
 const app = express();
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public"))
 const jsonParser = express.json();
 
 // Create
